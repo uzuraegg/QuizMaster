8543661f33c9af52ff8e432f3ab370c6cdaf4727
diesen <diesen.aruzu65@gmail.com>
2018-06-20 06:22:53 +0900

    create title activity

---
+++ app/src/main/java/com/diesen/quizmaster/TitleActivity.java
@@ -0,0 +1,17 @@
+package com.diesen.quizmaster;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class TitleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_title);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+}
